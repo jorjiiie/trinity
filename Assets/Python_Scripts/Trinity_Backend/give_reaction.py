c5ecf18652f9e9f7c97fbce65550c27e3c57e612
Luisa <103768169+luisa-li@users.noreply.github.com>
2023-09-16 19:08:24 -0400

    reorganized files

--- Assets/Python_Scripts/Trinity_Backend/give_reaction.py
+++ Assets/Python_Scripts/Trinity_Backend/give_reaction.py
@@ -13,13 +13,15 @@ def react(conversation: str, id1: str, id2: str) -> (str, str):
     score = sia.polarity_scores(conversation)['compound']
 
     print(score)
+    num = 5
 
     PDF = normal_distribution(0.8, score, 0.2)
 
     # normalize the pdf LOL
     PDF = PDF / np.sum(PDF)
 
-    x = np.random.
+    # can just replace the i for i ... with ju
+    x = np.random.choice([i for i in range(num)],size=1, p = PDF)
 
     # figure out what reactions go where and are associated with what
     num_reactions  = 5 # set this in actual code

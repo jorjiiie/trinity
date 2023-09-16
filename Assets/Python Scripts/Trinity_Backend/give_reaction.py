b5e498858cb612737d5cabc2a967f1b6fcc17c0a
Luisa <103768169+luisa-li@users.noreply.github.com>
2023-09-16 19:01:36 -0400

    Scores for sentiments

---
+++ Assets/Python Scripts/Trinity_Backend/give_reaction.py
@@ -0,0 +1,39 @@
+import nltk
+import numpy as np
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+def react(conversation: str, id1: str, id2: str) -> (str, str):
+    """"Given a conversation and two people's ids, calculate their reactions"""
+
+    def normal_distribution(x , mean , sd):
+        prob_density = (np.pi*sd) * np.exp(-0.5*((x-mean)/sd)**2)
+        return prob_density
+
+    sia = SentimentIntensityAnalyzer()
+    score = sia.polarity_scores(conversation)['compound']
+
+    print(score)
+
+    PDF = normal_distribution(0.8, score, 0.2)
+
+    # normalize the pdf LOL
+    PDF = PDF / np.sum(PDF)
+
+    x = np.random.
+
+    # figure out what reactions go where and are associated with what
+    num_reactions  = 5 # set this in actual code
+
+    
+    
+
+
+
+react("hows your day! it's pretty good i am having fun", "a", "b")
+    
+
+
+
+    
+    
+    

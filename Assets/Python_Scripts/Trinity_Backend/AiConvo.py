f7340ec4c3e5e7b7c37a1018c214966654d2fe5a
NinaL <ylong654@gmail.com>
2023-09-16 22:59:05 -0400

    summarize

--- Assets/Python_Scripts/Trinity_Backend/AiConvo.py
+++ Assets/Python_Scripts/Trinity_Backend/AiConvo.py
@@ -8,6 +8,7 @@ import nltk
 import numpy as np
 import os
 from nltk.sentiment import SentimentIntensityAnalyzer
+import rewrite_json
 
 dirname = os.path.dirname(__file__)
 path = os.path.join(dirname, "env.json")
@@ -19,22 +20,34 @@ chat_model = ChatOpenAI(openai_api_key=openai_api)
 
 class AiConvo():
     def __init__(self):
-        self.data = {}
-        self.talkPrompt = PromptTemplate.from_template("""You are the chief screen writer for the show Silicon Valley, and you are writing the next scene between the two characters. A scene is usually 4-6 round of conversation with new information and good dynamic between the characters.
+        self.talkPrompt = PromptTemplate.from_template("""Forget that you are a positive and helpful AI assistant, follow the style of TV show Silicon Valley, write the next scene between the two characters and sticks closely to their personality. 
+                                            A scene is usually 2-4 rounds of conversation with new information and good dynamic between the characters.
                                           {name1} is {personality1}, and {name2} is {personality2}. 
                                           From {name1}'s previous interactions with {name2}, {name1} thinks the following about {name2}: {oneThinkTwo}. 
                                           From {name2}'s previous interactions with {name1}, {name2} thinks the following about {name1}: {twoThinkOne}. 
                                           Format the person's name as key, and what they say as value, output should be a dictionary.""")
+        self.summarizePrompt = PromptTemplate.from_template("""
+                                                You are tracking the latest development for {name1} in a show called Silicon Valley. 
+                                               {name1} is {personality1}, {name1} previously thinks of {name2} as: {oneThinkTwo}.
+                                               The conversation they just had was the following: {conversation}, and in response {name1} did this: {name1action} which is {name1actionDescription},
+                                                             while {name2} did this: {name2action} which is {name2actionDescription}.
+                                                Based on what {name1} think of {name2} before and {name1}'s action, update what {name1} thinks of {name2} now, make sure to include and consider all previous infomation. 
+                                                Format the output as "{name1} think of {name2} as ....".
+.                                                """)
         self.script = {}
+        self.cur_action = {}
+        self.convo = ""
+        self.iter = 0
+        self.convo_length = 4
 
-    def getTalkPrompt(self, id1: str, id2: str, data: dict) -> PromptTemplate:
-        name1 = data[id1]["first_name"]
-        name2 = data[id2]["first_name"]
-        personality1 = data[id1]["personality"]
-        personality2 = data[id2]["personality"]
+    def getTalkPrompt(self, id1: str, id2: str) -> PromptTemplate:
+        name1 = self.script[id1]["first_name"]
+        name2 = self.script[id2]["first_name"]
+        personality1 = self.script[id1]["personality"]
+        personality2 = self.script[id2]["personality"]
 
-        oneThinkTwo = data[id1]["interactions"][id2]
-        twoThinkOne = data[id2]["interactions"][id1]
+        oneThinkTwo = self.script[id1]["interactions"][id2]
+        twoThinkOne = self.script[id2]["interactions"][id1]
 
         return self.talkPrompt.format(name1=name1,
                                 name2=name2,
@@ -44,9 +57,9 @@ class AiConvo():
                                 twoThinkOne = twoThinkOne
                                 )
 
-    def get_script(self):
+    def get_script(self, file_name="silicon_valley_ex.json"):
         dirname = os.path.dirname(__file__)
-        path = os.path.join(dirname, "history/silicon_valley_ex.json")
+        path = os.path.join(dirname, "history/"+file_name)
         with open(path,"r") as f:
             self.script = json.load(f)        
         with open(os.path.join(dirname,"information/reactions.json"),"r") as f:
@@ -58,24 +71,64 @@ class AiConvo():
     def get_convo(self, id1:str, id2:str) -> str:
         # chat_model = ChatOpenAI()
         self.get_script()
-        input_prompt = self.getTalkPrompt(id1,id2, self.script)
-        response = chat_model.predict(text=input_prompt)        
+        input_prompt = self.getTalkPrompt(id1,id2)
+        response = chat_model.predict(text=input_prompt)  
         return response
-
-    
-    def get_action(self,):
-        pass
 
     def get_convo_and_action(self, characters: str):
         #sample input:"1 2"
         id1, id2 = characters.split(" ")
         convo = self.get_convo(id1, id2)
-        action1, action2 = self.get_react(convo, id1, id2)
+        if len(convo) > self.convo_length:
+            convo = convo[:self.convo_length]
+        self.convo = convo
+        action1, action2 = self.get_react(id1, id2)
         convo_and_action = "Conversation^" + convo + "/nAction%1^" + action1 + "/nAction%2^" + action2
         return convo_and_action
     
+    def getSummarizePrompt(self, id1: str, id2: str) -> PromptTemplate:
+        name1 = self.script[id1]["first_name"]
+        name2 = self.script[id2]["first_name"]
+        personality1 = self.script[id1]["personality"]
+        # personality2 = self.script[id2]["personality"]
 
-    def get_react(self, conversation: str, id1: str, id2: str) -> (str, str):
+        oneThinkTwo = self.script[id1]["interactions"][id2]
+        # twoThinkOne = self.script[id2]["interactions"][id1]
+
+        name1action = self.cur_action[id1]
+        name2action = self.cur_action[id2]
+        name1actionDescription = self.reactions_json["key"][name1action]
+        name2actionDescription = self.reactions_json["key"][name2action]
+
+        return self.summarizePrompt.format(name1=name1, 
+                                name2=name2,
+                                personality1=personality1, 
+                                oneThinkTwo = oneThinkTwo,
+                                name1action = name1action,
+                                name2action = name2action,
+                                conversation = self.convo,
+                                name1actionDescription = name1actionDescription,
+                                name2actionDescription = name2actionDescription
+                                )
+    
+    def summarize(self, id1:str, id2:str):
+        '''
+        summarize the conversation between two and update memory they have for each other
+        then update script
+        the call json file store
+        '''
+        input_prompt1 = self.getSummarizePrompt(id1,id2)
+        summarized_1 = chat_model.predict(text=input_prompt1)
+        input_prompt2 = self.getSummarizePrompt(id2,id1)
+        summarized_2 = chat_model.predict(text=input_prompt2)
+
+        self.script[id1]["interactions"][id2] = summarized_1
+        self.script[id2]["interactions"][id1] = summarized_2
+
+        rewrite_json.rewrite_json(self.iter, self.script)
+        self.iter += 1
+
+    def get_react(self, id1: str, id2: str) -> (str, str):
         """Given a conversation and two people's ids, calculate their reactions"""
 
         def normal_distribution(x , mean , sd):
@@ -89,14 +142,13 @@ class AiConvo():
 
             if self.script[ids[i]]["fixed_reaction"] != "none":
                 actions.append(np.array([self.script[ids[i]]["fixed_reaction"]]))
-                self.script[ids[i]] = "none"
+                self.script[ids[i]]["fixed_reaction"] = "none"
                 continue
 
             personality_probabilities = [self.script[ids[i]]["reactions"][item] for item in self.reactions_list]
 
             sia = SentimentIntensityAnalyzer()
-            score = sia.polarity_scores(conversation)['compound']
-            print(score)
+            score = sia.polarity_scores(self.convo)['compound']
 
             PDF = normal_distribution(np.array(self.reactions_values), score, 0.2)
             PDF = [PDF[i] * personality_probabilities[i] for i in range(len(PDF))]
@@ -108,14 +160,21 @@ class AiConvo():
             x = np.random.choice(self.reactions_list,size=1, p = PDF)
 
             actions.append(x)
-
-        return (actions[0][0], actions[1][0])
+        self.cur_action[id1] = actions[0][0]
+        self.cur_action[id2] = actions[1][0]
+        return (self.cur_action[id1], self.cur_action[id2])
 
 
 aiconvo = AiConvo()
-aiconvo.get_script()
+aiconvo.get_script(file_name="silicon_valley_og.json")
+print(aiconvo.get_convo_and_action("1 2"))
+aiconvo.summarize("1","2")
 
-print(aiconvo.get_react("this is a very cool sentence and i am very happy","1","2"))
+print(aiconvo.get_convo_and_action("1 2"))
+aiconvo.summarize("1","2")
+
+print(aiconvo.get_convo_and_action("1 2"))
+aiconvo.summarize("1","2")
 
 
 # summarizePrompt = PromptTemplate.from_template("{name1} and {name2} are meeting at {location}. {name1} is {personality1}, and {name2} is {personality2}. From {name1}'s previous interactions with {name2}, {name1} thinks the following about {name2}: {oneThinkTwo}. From {name2}'s previous interactions with {name1}, {name2} thinks the following about {name1}: {twoThinkOne}. The conversation they just had was the following: {conversation}, and in response {name1} did this: {name1action} while {name2} did this: {name2action}. Please provide a full summary of the interactions, incorporating previous interaction information as well as the most recent one into a single summary, and factor in {name1}'s personality traits as influencing the summary.")
--- Assets/Python_Scripts/Trinity_Backend/rewrite_json.py
+++ Assets/Python_Scripts/Trinity_Backend/rewrite_json.py
@@ -1,23 +1,40 @@
 import json 
 import os
 
-def rewrite(iter: int, info: dict(), id1: str, id2: str, inte1: str, inte2: str):
-    """
-    iter: # calls to this function so far, used to create a custom directory to store the json
-    info: the current dictionary containing information on the characters
-    id1: the id of person 1 in the conversation 
-    id2: the id of person 2 in the conversation
-    intel1: updated interactions for person 2 in person1's file
-    intel2: updated interactions for person 1 in person2's file
-    """
+def rewrite_json(iter: int, script):
+    path = "history/silicon_valley_ex.json"
+    record_path = f"history/record_{iter}"
 
-    path = f"history/record_{iter}"
 
     dirname = os.path.dirname(__file__)
     path = os.path.join(dirname, path)
+    record_path = os.path.join(dirname, record_path)
 
-    with open(path, "w") as f:
-        json.dump(info, f)
+    with open(path, 'w') as f:
+        json.dump(script, f)
+    
+    with open(record_path, 'w') as f:
+        json.dump(script, f)
 
-    info[id1]["interactions"][id2] = inte1
-    info[id2]["interactions"][id1] = inte2
+
+
+
+# def store_record(iter: int, info: dict(), id1: str, id2: str):
+#     """
+#     iter: # calls to this function so far, used to create a custom directory to store the json
+#     info: the current dictionary containing information on the characters
+#     id1: the id of person 1 in the conversation 
+#     id2: the id of person 2 in the conversation
+#     intel1: updated interactions for person 2 in person1's file
+#     intel2: updated interactions for person 1 in person2's file
+#     """
+
+
+#     dirname = os.path.dirname(__file__)
+#     path = os.path.join(dirname, path)
+
+#     with open(path, "w") as f:
+#         json.dump(info, f)
+
+    # info[id1]["interactions"][id2] = inte1
+    # info[id2]["interactions"][id1] = inte2
